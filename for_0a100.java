e72ae2393340257187925211c921e8afa0b16084
Ricardo <ricardoarturodavalos12@gmail.com>
2017-10-26 20:02:19 -0500

    programita en for de multiplos de 5 de 0 a 100

---
+++ for_0a100.java
@@ -0,0 +1,12 @@
+//programa que realiza la serie de 0 a 100
+class for_0a100{
+    public static void main(String[] args){
+        int i;
+
+        for(i = 0;i<=100;i++){
+            if(i % 5 == 0){
+                System.out.print(i + " , ");
+            }
+        }
+    }
+}
